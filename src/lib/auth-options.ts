e9cb80edfdf36e1ff12345d7a84e80c3d067e019
Felipe <felipeabreu.1994@gmail.com>
2026-04-13 17:54:24 -0300

    feat: configura suporte para base de dados Neon e Vercel

--- src/lib/auth-options.ts
+++ src/lib/auth-options.ts
@@ -12,22 +12,16 @@ export const authOptions: NextAuthOptions = {
         password: { label: "Senha", type: "password" }
       },
       async authorize(credentials) {
-        console.log("CREDENTIALS:", credentials)
-
         if (!credentials?.email || !credentials?.password) {
-          console.log("❌ Credenciais ausentes")
           return null
         }
 
         const user = await prisma.usuario.findUnique({
           where: { email: credentials.email },
-          include: { role: true } // 🔥 OBRIGATÓRIO: Traz a tabela Role junto!
+          include: { role: true }
         })
 
-        console.log("USER:", user)
-
         if (!user) {
-          console.log("❌ Usuário não encontrado")
           return null
         }
 
@@ -36,36 +30,29 @@ export const authOptions: NextAuthOptions = {
           user.senha
         )
 
-        console.log("PASSWORD OK:", isValid)
-
         if (!isValid) {
-          console.log("❌ Senha inválida")
           return null
         }
 
         // Prepara a devolução do usuário
         const cargo = user.role?.nome || "MEMBRO";
-        const isMaster = user.is_master === true; // Garante o booleano
+        const isMaster = user.is_master === true; 
         
-        const userReturn = {
+        return {
           id: user.id,
           name: user.nome,
           email: user.email,
           igreja_id: user.igreja_id,
           role: cargo,
           permissions: isMaster ? ["*"] : [],
-          is_master: isMaster // 🔥 AGORA SIM ELE VAI JUNTO!
-        }
-        
-        console.log("✅ RETORNANDO USER:", userReturn);
-        return userReturn;
+          is_master: isMaster 
+        };
       }
     })
   ],
   session: { strategy: "jwt" },
   callbacks: {
     async jwt({ token, user }) {
-      // Ocorre logo após o login. Pega o userReturn e joga no Token
       if (user) {
         token.igreja_id = (user as any).igreja_id;
         token.is_master = (user as any).is_master;
@@ -75,7 +62,6 @@ export const authOptions: NextAuthOptions = {
       return token;
     },
     async session({ session, token }) {
-      // Ocorre a cada requisição. Pega o Token e joga na Sessão ativa
       if (session.user) {
         (session.user as any).igreja_id = token.igreja_id;
         (session.user as any).is_master = token.is_master;
